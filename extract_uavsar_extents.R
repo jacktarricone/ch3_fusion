d4791286cd303109fd28510ee85d1e12afb5c40b
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-07-18 12:16:31 -0600

    work from hackweek

---
+++ extract_uavsar_extents.R
@@ -0,0 +1,27 @@
+# convert uavsar coherence data to shape files for isce_sat2 group work
+# july 13th, 2022
+# we have failed the group. we are using R
+
+library(terra)
+
+list_coherence_files <-list.files("/Users/jacktarricone/hackweek2022/isce_sat2/coherence", full.names = TRUE)
+print(list_coherence_files)
+
+# create shape file from cor data
+rast_to_shp <-function(file){
+
+  rast <-rast(file) #import raster
+  name <-names(rast) # extract raster name
+  rast[rast > 0] <- 1   # convert all values to 1
+  rast_shp_file <-as.polygons(rast) # convert to vector data
+  
+  ## aggregate polyongs up to just data extent
+  rast_shp <- aggregate(rast_shp_file, dissolve = TRUE, fun = "mean", cores = 10)
+  
+  setwd("/Users/jacktarricone/hackweek2022/isce_sat2/") # setwd
+  writeVector(rast_shp, paste(name,".shp")) # save
+  return(rast_shp)
+}
+
+# apply our funciton to the list of rasters
+lapply(list_coherence_files, rast_to_shp)
