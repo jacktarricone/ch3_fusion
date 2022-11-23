2762bd7b7c008c1a895c097c666951f32ea24743
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-11-23 10:47:40 -0700

    cropping ya feel

--- sen2_ndsi_download.R
+++ sen2_ndsi_download.R
@@ -1,3 +1,6 @@
 library(sen2r)
 sen2r()
 # s2_order("/Users/jacktarricone/.sen2r/lta_orders/lta_20221118_121440.json")
+
+s2_order("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json")
+safe_is_online("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json")
