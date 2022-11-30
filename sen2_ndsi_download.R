95d47b5d40deae5252db3d1a63a7fefa67d9008f
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-11-30 08:17:00 -0800

    sen2

--- sen2_ndsi_download.R
+++ sen2_ndsi_download.R
@@ -2,5 +2,7 @@ library(sen2r)
 sen2r()
 # s2_order("/Users/jacktarricone/.sen2r/lta_orders/lta_20221118_121440.json")
 
-s2_order("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json")
+s2_order("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json", service = "dhus")
 safe_is_online("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json")
+
+s2_download("/Users/jacktarricone/.sen2r/lta_orders/lta_20221122_083828.json", service = "dhus")
