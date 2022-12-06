7c289c1f498ff5d06681ed9f49fceed0c84c9ccd
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-12-06 11:53:46 -0800

    pushing

--- sierra_swe_calc.R
+++ sierra_swe_calc.R
@@ -3,6 +3,7 @@
 # decemeber 5, 2022
 
 library(terra)
+library(ggplot2);theme_set(theme_classic(12))
 
 setwd("/Users/jacktarricone/ch3_fusion/rasters/uavsar/feb26_march11_80m/")
 list.files()
@@ -43,6 +44,54 @@ devtools::source_url("https://raw.githubusercontent.com/jacktarricone/snowex_uav
 #   
 # }
 
+##### 
+## modscag
+#####
+
+modscag_depth_change <-depth_from_phase(delta_phase = unw_modscag,
+                                        inc_angle = inc,
+                                        perm = 1.3,
+                                        wavelength = uavsar_wL)
+
+# convert to SWE change
+modscag_dswe_raw <-modscag_depth_change*(270/1000)
+plot(modscag_dswe_raw)
+hist(modscag_dswe_raw , breaks = 100)
+
+
+##### 
+## modis
+#####
+
+modis_depth_change <-depth_from_phase(delta_phase = unw_modis,
+                                        inc_angle = inc,
+                                        perm = 1.3,
+                                        wavelength = uavsar_wL)
+
+# convert to SWE change
+modis_dswe_raw <-modis_depth_change*(270/1000)
+plot(modis_dswe_raw)
+hist(modis_dswe_raw , breaks = 100)
+
+##### 
+## viirs
+#####
+
+viirs_depth_change <-depth_from_phase(delta_phase = unw_viirs,
+                                      inc_angle = inc,
+                                      perm = 1.3,
+                                      wavelength = uavsar_wL)
+
+# convert to SWE change
+viirs_dswe_raw <-viirs_depth_change*(270/1000)
+plot(viirs_dswe_raw)
+hist(viirs_dswe_raw , breaks = 100)
+
+
+
+
+
+
 # testing
 flm_depth_change <-depth_from_phase(delta_phase = unw_flm,
                                 inc_angle = inc,
@@ -65,4 +114,28 @@ modis_depth_change <-depth_from_phase(delta_phase = unw_modis,
 modis_dswe_raw <-modis_depth_change*(270/1000)
 plot(modis_dswe_raw)
 hist(modis_dswe_raw, breaks = 100)
-writeRaster(modis_dswe_raw, "./modis_dswe_raw.tif")
+#writeRaster(modis_dswe_raw, "./modis_dswe_raw.tif")
+
+
+
+
+
+# bring in snow pillow data
+vlc <-read.csv("/Users/jacktarricone/ch3_fusion/in_situ/VOLCANIC KNOB (VLC).csv")
+colnames(vlc)[1:2] <-c("date","swe_in")
+vlc$date <-lubridate::mdy(vlc$date)
+vlc$swe_in <-as.numeric(vlc$swe_in)
+vlc$swe_cm <-as.numeric(vlc$swe_in *2.54)
+
+# test plot from vlc cadwr pillow
+ggplot(vlc) +
+  geom_line(aes(x = date, y = swe_cm))
+
+# study period filter
+sp <-dplyr::filter(vlc, date > "2020-02-26" & date < "2020-03-11")
+
+ggplot(sp) +
+  geom_line(aes(x = date, y = swe_cm))
+
+# calc change in SWE at pillow
+insitu_dswe <-sp$swe_cm[13] - sp$swe_cm[1]
