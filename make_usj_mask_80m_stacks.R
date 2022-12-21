881ab4a2064feb00309592aaad0bd365f6c2f07c
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-12-20 22:12:23 -0500

    looping cropping

--- make_usj_mask_80m_stacks.R
+++ make_usj_mask_80m_stacks.R
@@ -32,14 +32,15 @@ modis_stack
 # pull out modis snow cover
 sc_seq <-seq(1,1825,5)
 modis_fsca <-modis_stack[[sc_seq]]
-modis_fsca
+modis_fsca_crop <-crop(modis_fsca, cor)
+values(modis_fsca_crop)[values(modis_fsca_crop) < 15] = NA # mask for %15 fsca
+modis_fsca_crop
 
 # resample and mask
-modis_nisar_v1 <-resample(modis_fsca, cor, method = "bilinear")
+modis_nisar_v1 <-resample(modis_fsca_crop, cor, method = "bilinear")
 modis_fsca_usj_80m <-mask(modis_nisar_v1, usj)
 modis_fsca_usj_80m
-plot(modis_fsca_usj_80m[[20]])
-writeRaster(modis_fsca_usj_80m, "./MOD10A1F_wy2020/fsca_usj_80m/modis_fsca_usj_80m_stack.tif")
+writeRaster(modis_fsca_usj_80m, "./fsca_usj_80m/modis_fsca_usj_80m_stack.tif")
 
 #####################
 ###### viirs ########
@@ -53,14 +54,14 @@ viirs_stack
 # pull out viirs snow cover
 sc_seq <-seq(3,1828,5)
 viirs_fsca <-viirs_stack[[sc_seq]]
-viirs_fsca
-plot(viirs_fsca[[180]])
+viirs_fsca_crop <-crop(viirs_fsca, cor)
+values(viirs_fsca_crop)[values(viirs_fsca_crop) < 15] = NA # mask for 15% fsca
+viirs_fsca_crop
 
 # resample and mask
-viirs_nisar_v1 <-resample(viirs_fsca, cor, method = "bilinear")
+viirs_nisar_v1 <-resample(viirs_fsca_crop, cor, method = "bilinear")
 viirs_fsca_usj_80m <-mask(viirs_nisar_v1, usj)
 viirs_fsca_usj_80m
-plot(viirs_fsca_usj_80m[[20]])
 writeRaster(viirs_fsca_usj_80m, "./fsca_usj_80m/viirs_fsca_usj_80m_stack.tif")
 
 #####################
@@ -76,7 +77,7 @@ for (i in 1:length(flm_list)){
   
   r <-flm_rast[[i]] # bring in rast
   r_c <-crop(r, ext(cor_utm)) # crop, this makes things much better
-  values(r_c)[values(r_c) < 15] = NA # mask for 
+  values(r_c)[values(r_c) < 15] = NA # mask for 15% fsca
   reproj <-project(r_c, "EPSG:4326", method = "bilinear") # reproject
   usj_clip <-resample(reproj, cor, method = "bilinear") # resample to NISAR usj extent
   usj_mask <-mask(usj_clip, usj) # mask for usj
@@ -92,16 +93,20 @@ flm_stack <-rast(flm_formatted)
 writeRaster(flm_stack, "/Users/jacktarricone/ch3_fusion/rasters/fsca_usj_80m/flm_fsca_usj_80m_stack.tif")
 
 
-flm_stack <-rast(flm_list)
-flm_stack
-plot(flm_stack[[70]])
+#####################
+###### modscag ########
+#####################
 
-# resample, reproject, and mask
-flm_p1 <-project(flm_stack[[1:30]], "EPSG:4326", method = "bilinear")
+# # bring in modscag stack
+# modscag_list <-list.files("./modscag/", pattern = '.tif', full.names = TRUE)
+# modscag_stack <-rast(modscag_list)
+# values(modscag_stack)[values(modscag_stack) < 15] = NA # mask for 
+# modscag_stack
+# 
+# # resample and mask
+# modscag_nisar_v1 <-resample(modscag_stack, cor, method = "bilinear")
+# modscag_fsca_usj_80m <-mask(modscag_nisar_v1, usj)
+# modscag_fsca_usj_80m
+# plot(modscag_fsca_usj_80m[[20]])
+# writeRaster(modscag_fsca_usj_80m, "./fsca_usj_80m/modscag_fsca_usj_80m_stack.tif")
 
-values(flm)[values(flm) < 15] = NA
-flm_nisar_v1 <-resample(flm, cor, method = "bilinear")
-flm_fsca_usj_80m <-mask(flm_nisar_v1, usj)
-flm_fsca_usj_80m
-plot(flm_fsca_usj_80m[[20]])
-writeRaster(flm_fsca_usj_80m, "./fsca_usj_80m/flm_fsca_usj_80m_stack.tif")
