114622bb9b63bfc4ff0b9d1e5f16a79dd4d6f8e3
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-06-17 13:32:25 -0700

    smh

--- format_cadwr_uavsar_data.R
+++ format_cadwr_uavsar_data.R
@@ -21,13 +21,24 @@ head(sd_df)
 # rename swe cols
 new_names <-c("col_label","id","duration","sensor_numnber","snow_wc","date_time","date_raw","swe_in","flag","units")
 colnames(swe_df) <-new_names
+swe_df$date <-ymd(swe_df$date_raw)
+swe_df <-dplyr::select(swe_df, c("id", "date", "swe_in"))
 
 # sd
 new_names <-c("col_label","id","duration","sensor_numnber","snow_dp","date_time","date_raw","snow_depth_in","flag","units")
 colnames(sd_df) <-new_names
+sd_df$date <-ymd(sd_df$date_raw)
+sd_df <-dplyr::select(sd_df, c("id", "date", "snow_depth_in"))
 
 # join
-joined_df <-full_join(swe_df,sd_df)
+identical(sd_df$date, swe_df$date)
+test <-ifelse(sd_df$date == swe_df$date, "TRUE", "FALSE")
+unique(test)
+nans <-which(test == is.na(test))
+
+joined_df <-full_join(swe_df,sd_df, by = c("id", "date"))
+
+test <-filter(joined_df, id == "DPO")
 joined_df$date <-ymd(joined_df$date_raw)
 joined_df <-dplyr::select(joined_df, c("id", "date", "flag", "swe_in", "snow_depth_in"))
 head(joined_df)
