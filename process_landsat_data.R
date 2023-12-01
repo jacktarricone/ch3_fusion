78e3d796475a30a35428bfd7d2b9a0ddf702c210
Jack Tarricone <jack.tarricone@nasa.gov>
2023-12-01 17:32:34 -0500

    eod

--- process_landsat_data.R
+++ process_landsat_data.R
@@ -13,33 +13,33 @@ coh_80m <-rast("./new_uavsar/p1_80m/p1_14d_VV_coh_80m.tif")
 
 
 ############################
-########## feb 11 ##########
+########## feb 01 ##########
 ############################
 
 # landsat
 # bring in other tile so it covers full basin
-l_0217_10 <-rast("./landsat_fsca/h3_v10_2020/LC08_CU_003010_20200210_20210504_02_SNOW/LC08_CU_003010_20200210_20210504_02_GROUND_SNOW.TIF")
-l_0217_09 <-rast("./landsat_fsca/h3_v09_2020/LC08_CU_003009_20200210_20210504_02_SNOW/LC08_CU_003009_20200210_20210504_02_GROUND_SNOW.TIF")
+l_0201_10 <-rast("./landsat_fsca/h3_v10_2020/LC08_CU_003010_20200201_20210504_02_SNOW/LC08_CU_003010_20200201_20210504_02_GROUND_SNOW.TIF")
+l_0201_09 <-rast("./landsat_fsca/h3_v09_2020/LC08_CU_003009_20200201_20210504_02_SNOW/LC08_CU_003009_20200201_20210504_02_GROUND_SNOW.TIF")
 
 # merge and reproject
-l_0217 <-merge(l_0217_10, l_0217_09)
-l_0217_v1 <-l_0217/10
-plot(l_0217)
+l_0201 <-merge(l_0201_10, l_0201_09)
+l_0201_v1 <-l_0201/10
+plot(l_0201)
 
 # reproj
-l_0217_v2 <-project(l_0217_v1, "EPSG:4326", method = "bilinear")
-l_0217_v2
-plot(l_0217_v1)
+l_0201_v2 <-project(l_0201_v1, "EPSG:4326", method = "bilinear")
+l_0201_v2
+plot(l_0201_v1)
 
 # crop and mask to 
-l_0217_v3 <-crop(mask(l_0217_v2, sierra),ext(sierra))
-plot(l_0217_v3)
-writeRaster(l_0217_v3, "./new_optical/p2_80m_20200212_20200219/landsat_fsca_30m_20200217.tif")
+l_0201_v3 <-crop(mask(l_0201_v2, sierra),ext(sierra))
+plot(l_0201_v3)
+# writeRaster(l_0201_v3, "./new_optical/p1_80m_20200131_20200212/landsat_fsca_30m_20200201.tif")
 
 # resample to 
-l_0217_80m <-resample(l_0217_v3, coh_80m, method = 'bilinear')
-plot(l_0217_80m)
-writeRaster(l_0217_80m, "./new_optical/p2_80m_20200212_20200219/landsat_fsca_80m_20200217.tif")
+l_0201_80m <-resample(l_0201_v3, coh_80m, method = 'bilinear')
+plot(l_0201_80m)
+# writeRaster(l_0201_80m, "./new_optical/p1_80m_20200131_20200212/landsat_fsca_80m_20200201.tif")
 
 
 
@@ -71,3 +71,33 @@ plot(l_0217_v3)
 l_0217_80m <-resample(l_0217_v3, coh_80m, method = 'bilinear')
 plot(l_0217_80m)
 # writeRaster(l_0217_80m, "./new_optical/p2_80m_20200212_20200219/landsat_fsca_80m_20200217.tif")
+
+
+############################
+########## mar 04 ##########
+############################
+
+# landsat
+# bring in other tile so it covers full basin
+l_0304_10 <-rast("./landsat_fsca/h3_v10_2020/LC08_CU_003010_20200304_20210504_02_SNOW/LC08_CU_003010_20200304_20210504_02_GROUND_SNOW.TIF")
+l_0304_09 <-rast("./landsat_fsca/h3_v09_2020/LC08_CU_003009_20200304_20210504_02_SNOW/LC08_CU_003009_20200304_20210504_02_GROUND_SNOW.TIF")
+
+# merge and reproject
+l_0304 <-merge(l_0304_10, l_0304_09)
+l_0304_v1 <-l_0304/10
+plot(l_0304)
+
+# reproj
+l_0304_v2 <-project(l_0304_v1, "EPSG:4326", method = "bilinear")
+l_0304_v2
+plot(l_0304_v1)
+
+# crop and mask to 
+l_0304_v3 <-crop(mask(l_0304_v2, sierra),ext(sierra))
+plot(l_0304_v3)
+# writeRaster(l_0304_v3, "./new_optical/p4_80m_20200226_20200311/landsat_fsca_30m_20200304.tif")
+
+# resample to 
+l_0304_80m <-resample(l_0304_v3, coh_80m, method = 'bilinear')
+plot(l_0304_80m)
+# writeRaster(l_0304_80m, "./new_optical/p4_80m_20200226_20200311/landsat_fsca_80m_20200304.tif")
