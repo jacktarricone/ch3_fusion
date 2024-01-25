57b660d6cc5eba4a0e1b8ea82c9b31dcae203d5f
Jack Tarricone <jack.tarricone@nasa.gov>
2024-01-25 15:27:58 -0500

    plots made

--- plot_swe_barplot.R
+++ plot_swe_barplot.R
@@ -1,15 +1,9 @@
-# plot dswe data in grid
+# make dswe stats and bar plot
+# jack tarricone
+# jan 25, 2024
 
 library(terra)
 library(ggplot2)
-library(RColorBrewer)
-library(sf)
-library(cowplot)
-library(scales)
-library(viridis)
-library(ggpubr)
-library(data.table)
-library(tidyr)
 
 theme_classic <- function(base_size = 11, base_family = "",
                           base_line_size = base_size / 22,
@@ -40,18 +34,17 @@ theme_classic <- function(base_size = 11, base_family = "",
     )
 }
 
-theme_set(theme_classic(15))
 
-setwd("~/ch3_fusion/rasters")
+theme_set(theme_classic(18))
 
 # load in 80 m insar dswe products
-p1_stack_cm <-rast(list.files("./new_dswe/p1", full.names = T))
+p1_stack_cm <-rast(list.files("./rasters/new_dswe/p1", full.names = T))
 names(p1_stack_cm) <-c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS")
-p2_stack_cm <-rast(list.files("./new_dswe/p2", full.names = T))
+p2_stack_cm <-rast(list.files("./rasters/new_dswe/p2", full.names = T))
 names(p2_stack_cm) <-c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS")
-p3_stack_cm <-rast(list.files("./new_dswe/p3", full.names = T))
+p3_stack_cm <-rast(list.files("./rasters/new_dswe/p3", full.names = T))
 names(p3_stack_cm) <-c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS")
-p4_stack_cm <-rast(list.files("./new_dswe/p4", full.names = T))
+p4_stack_cm <-rast(list.files("./rasters/new_dswe/p4", full.names = T))
 names(p4_stack_cm) <-c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS")
 
 # create cell size raster in m^2
@@ -66,25 +59,25 @@ p4_stack_m3 <-(p4_stack_cm/10 * cell_size_m2)*1e-3
 
 ### format data.frames for plotting
 p1_df <-as.data.frame(p1_stack_m3, xy = TRUE)
-p1_df$pair <-rep("P1", nrow(p1_df))
+p1_df$pair <-rep("(a) P1", nrow(p1_df))
 p1_df_l <-pivot_longer(p1_df, 
                        cols = c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS"),
                        names_to = c("data_set"))
 
 p2_df <-as.data.frame(p2_stack_m3, xy = TRUE)
-p2_df$pair <-rep("P2", nrow(p2_df))
+p2_df$pair <-rep("(b) P2", nrow(p2_df))
 p2_df_l <-pivot_longer(p2_df, 
                        cols = c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS"),
                        names_to = c("data_set"))
 
 p3_df <-as.data.frame(p3_stack_m3, xy = TRUE)
-p3_df$pair <-rep("P3", nrow(p3_df))
+p3_df$pair <-rep("(c) P3", nrow(p3_df))
 p3_df_l <-pivot_longer(p3_df, 
                        cols = c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS"),
                        names_to = c("data_set"))
 
 p4_df <-as.data.frame(p4_stack_m3, xy = TRUE)
-p4_df$pair <-rep("P4", nrow(p4_df))
+p4_df$pair <-rep("(d) P4", nrow(p4_df))
 p4_df_l <-pivot_longer(p4_df, 
                        cols = c("FLM","IMS","Landsat","MODIS","MODSCAG","VIIRS"),
                        names_to = c("data_set"))
@@ -92,51 +85,51 @@ p4_df_l <-pivot_longer(p4_df,
 
 # bind for plotting
 plotting_df <-rbind(p1_df_l,p2_df_l,p3_df_l,p4_df_l)
+colnames(plotting_df)[5] <-"dswe_m3"
+plotting_df <-na.omit(plotting_df)
 
-# read in sierra shp
-sierra_v1 <-st_read("~/ch3_fusion/shapefiles/sierra_multiseg_shp.gpkg")
-sierra_sf <-st_geometry(sierra_v1)
+# summarize for bar plots
+bar_results <- as.data.frame(plotting_df %>%
+  group_by(pair,data_set) %>%
+  summarize(
+    Loss = as.integer(sum(dswe_m3[dswe_m3 < 0])*1e-3),
+    Gain = as.integer(sum(dswe_m3[dswe_m3 > 0])*1e-3),
+    Net = as.integer(sum(dswe_m3)*1e-3)))
 
-############
-### plot
-############
+print(bar_results)
 
-# set scolor scale
-# cc scale
-dswe_scale <-brewer.pal(9, "RdBu")
+# pivot longer again for plotting
+bar_plotting <- bar_results %>%
+  pivot_longer(cols = c("Loss", "Gain", "Net"), names_to = "stat", values_to = "swe_change")
 
-p <-ggplot(plotting_df) +
-  geom_sf(data = sierra_sf, fill = "gray50", color = "black", linewidth = .1, inherit.aes = FALSE, alpha = 1) +
-  geom_raster(mapping = aes(x,y, fill = value)) + 
-  facet_grid(vars(pair), vars(data_set), scales = "fixed", switch = "y") +
-  scale_fill_gradientn(colors = dswe_scale, limits = c(-6,6), oob = squish, na.value = "gray50", guide = "none") + # max of color bar so it saturates
-  labs(fill = expression(Delta~SWE~(m^3~10^3))) +
-  theme(panel.border = element_blank(),
-        axis.text.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = "bottom",
-        plot.margin = unit(c(0,0,0,0), "cm"),
-        strip.background = element_blank(), 
-        legend.box.spacing = unit(0, "pt"), 
-        strip.text.y.left = element_text(angle = 0),
-        strip.text = element_text(size = 13, face = "bold")) +
-  guides(fill = guide_colorbar(direction = "horizontal",
-                               label.position = 'top',
-                               title.position ='bottom',
-                               title.hjust = .5,
-                               barwidth = 27,
-                               barheight = 1,
-                               frame.colour = "black", 
-                               ticks.colour = "black")) 
+bar_plotting
 
+# make group bar plot
+p <-ggplot(bar_plotting, aes(fill=stat, x = data_set, y=swe_change)) + 
+  geom_bar(position="dodge", stat="identity", color = "black", width = .5)+
+  # geom_text(aes(label=swe_change), position=position_dodge(width=0.9), vjust=-0.25)+
+  facet_wrap(~pair)+
+  geom_hline(yintercept = 0) +
+  scale_y_continuous(breaks = seq(-300,100,50),limits = c(-300,100))+
+  scale_fill_manual(values=c('darkblue','darkred','grey90'),name="")+
+  ylab(expression(Delta~SWE~(m^3~10^3))) +
+  xlab("fSCA Product") +
+  theme_classic(15) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1),
+        legend.position = "top",
+        legend.direction = "horizontal",
+        legend.justification = "center",
+        legend.box.just = "center", 
+        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+        legend.title = element_blank())
+
+# saves
 ggsave(p,
-       file = "~/ch3_fusion/plots/dswe_plot_v3.png",
-       width = 7, 
-       height = 12,
+       file = "~/ch3_fusion/plots/dswe_barplot_v2.png",
+       width = 10.5,
+       height = 6,
        dpi = 300)
 
-system("open ~/ch3_fusion/plots/dswe_plot_v3.png") 
+system('open ~/ch3_fusion/plots/dswe_barplot_v2.png')
+
 
