eb6bf2d957b1c1ce6bd3613f9c17644be5e15566
Jack Tarricone <jack.tarricone@nasa.gov>
2024-03-15 12:19:11 -0400

    multiseg lvk

--- sierra_swe_calc_p2.R
+++ sierra_swe_calc_p2.R
@@ -64,7 +64,7 @@ pillow_locations <-read.csv("~/ch3_fusion/csvs/cadwr_pillows_meta_uavsar_v1.csv"
 pillow_point <-vect(pillow_locations, geom = c("lon","lat"), crs = crs(unw_modis)) #needs to be 
 plot(dswe_raw[[1]])
 points(pillow_point, cex = 1)
-text(pillow_point, labels = c("VLC", "DPO", "MHP","UBC","WWC"), pos = 3)
+text(pillow_point, labels = c("VLC", "DPO", "MHP","UBC","WWC","PIT"), pos = 3)
 
 # calculate SWE change at pillow
 cadwr_swe <-read.csv("~/ch3_fusion/csvs/cadwr_swe_depth_qaqc_v1.csv")
@@ -81,15 +81,28 @@ ggplot(sp, aes(x = date, y = swe_cm, color = id)) +
   geom_line()
 
 # calc change in SWE at pillow from feb 12 - 19
-station_dswe <- sp %>%
+station_dswe_v1 <- sp %>%
   group_by(id) %>%
   summarize(dswe_cm = swe_cm[8] - swe_cm[1])
 
+station_dswe_v1
+
+# add pit diff
+pit_diff <-read.csv("~/ch3_fusion/csvs/mam_pit_diff.csv")
+colnames(pit_diff)[2:3] <-c("id","dswe_cm")
+pit_diff
+
+# select send row for p2
+p2_v1 <-pit_diff[2,]
+p2 <-subset(p2_v1, select = -X)
+
+# rbind
+station_dswe <-rbind(station_dswe_v1,p2)
 station_dswe
 
 # extract using that vector
 pillow_cell_dswe <-terra::extract(dswe_raw, pillow_point,  cells = TRUE, xy = TRUE, ID = TRUE)
-pillow_cell_dswe$id <-c("VLC", "DPO", "MHP","UBC","WWC")
+pillow_cell_dswe$id <-c("VLC", "DPO", "MHP","UBC","WWC","CUES")
 pillow_cell_dswe
 
 # extract 8 surronding cells
@@ -100,6 +113,7 @@ vlc_cells <-c(pillow_cell_dswe$cell[1],test_cells[1,])
 dpo_cells <-c(pillow_cell_dswe$cell[2],test_cells[2,])
 mhp_cells <-c(pillow_cell_dswe$cell[3],test_cells[3,])
 ubc_cells <-c(pillow_cell_dswe$cell[4],test_cells[4,])
+cues_cells <-c(pillow_cell_dswe$cell[6],test_cells[6,])
 
 # extract
 vlc_vals <-terra::extract(dswe_raw, vlc_cells)
@@ -110,19 +124,24 @@ mhp_vals <-terra::extract(dswe_raw, mhp_cells)
 colnames(mhp_vals) <-rep("mph", ncol(mhp_vals))
 ubc_vals <-terra::extract(dswe_raw, ubc_cells)
 colnames(ubc_vals) <-rep("ubc", ncol(ubc_vals))
+cues_vals <-terra::extract(dswe_raw, cues_cells)
+colnames(cues_vals) <-rep("cues", ncol(cues_vals))
+
 
 # make df
 vlc_mean <-mean(colMeans(vlc_vals, na.rm = TRUE), na.rm = TRUE)
 dpo_mean <-mean(colMeans(dpo_vals, na.rm = TRUE), na.rm = TRUE)
 mhp_mean <-mean(colMeans(mhp_vals, na.rm = TRUE), na.rm = TRUE)
 ubc_mean <-mean(colMeans(ubc_vals, na.rm = TRUE), na.rm = TRUE)
+cues_mean <-mean(colMeans(cues_vals, na.rm = TRUE), na.rm = TRUE)
+
 
 # mean station dswe
 mean_pillow_dswe <-mean(station_dswe$dswe_cm)
 mean_pillow_dswe
 
 # mean them all, pretty much the same
-mean_insar_dswe <-mean(c(vlc_mean,dpo_mean,mhp_mean,ubc_mean),na.rm = TRUE)
+mean_insar_dswe <-mean(c(vlc_mean,dpo_mean,mhp_mean,ubc_mean,cues_mean),na.rm = TRUE)
 mean_insar_dswe
 
 # create tether value
