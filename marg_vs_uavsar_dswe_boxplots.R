ae764d787af3cac99ce16478cfb7e7f1e0afb3c0
Jack Tarricone <jack.tarricone@nasa.gov>
2024-05-14 13:21:18 -0400

    boxplots doneish

--- marg_vs_uavsar_dswe_boxplots.R
+++ marg_vs_uavsar_dswe_boxplots.R
@@ -40,17 +40,20 @@ setwd("~/ch3_fusion/rasters/")
 
 # load in uavsar
 uavsar_list <-list.files("./wus_marg/pairs/", pattern = "uavsar", full.names = T)
-uavsar_stack <-rast(uavsar_list)
+uavsar_stack <-rast(uavsar_list) # convert back to meters
 
 # load in marg
 marg_list <-list.files("./wus_marg/pairs/", pattern = "v4", full.names = T)
-marg_stack <-rast(marg_list)*100
+marg_stack <-rast(marg_list)*100 # make rast in meters
+# marg_cs <-cellSize(marg_stack, unit = "m") # calc cell size in meters, it's variable
+# marg_stack_vol <-marg_stack*marg_cs # calculate volumetric swe change
 plot(marg_stack)
 
 # resample uavsar to 500m marg data
 uavsar_500_v1 <-resample(uavsar_stack, marg_stack, method = "bilinear")
 uavsar_500 <-mask(uavsar_500_v1, marg_stack)
-uavsar_500
+# uavsar_cs <-cellSize(uavsar_500, unit = "m") # calc cell size in meters, it's variable
+# uavsar_500_vol <-uavsar_500*uavsar_cs # calculate volumetric swe change
 plot(uavsar_500)
 
 # calc diff
@@ -89,132 +92,68 @@ diff_long <- diff_df %>%
 df1 <-bind_rows(marg_long,uavsar_long)
 df <-bind_rows(df1,diff_long)
 head(df)
-# data.table::fwrite(df, "~/ch3_fusion/csvs/uavsar_marg_plotting_df_GOOD_v2.csv")
-
-# calc stats
-stats_df <- df %>%
-  group_by(data, pair) %>%
-  summarize(mean_value = mean(dswe, na.rm = T),
-            sd_value = sd(dswe, na.rm = T))
-
-# plot
-p1 <-ggplot(df1, aes(x = pair, y = dswe, fill = data)) +
-  geom_hline(yintercept = 0, col = "gray50", linetype = 3)+
-  geom_boxplot(linewidth = .25, width = .4, 
-               outlier.shape = 1, outlier.color = 'red', outlier.alpha = .1, outlier.size = .2,
-               position = 'dodge') +
-  scale_fill_manual(values = c('UAVSAR' = 'goldenrod', 'WUS-SR' = 'purple2'))+
-  ylab(expression(Delta~SWE~(cm))) +
-  xlab("Pair") + 
-  theme(panel.border = element_rect(colour = "black", fill = NA, linewidth  = 1),
-        legend.position = c(.65,.9),
-        legend.direction = 'horizontal',
-        legend.title = element_blank(),
-        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
-        plot.margin = unit(c(.25,.25, 0,.25), "cm"))
-
-ggsave(p1,
-       file = "~/ch3_fusion/plots/marg_uavsar_diff_box_v4.png",
-       width = 6, 
-       height = 3,
-       units = "in",
-       dpi = 500) 
-
-system("open ~/ch3_fusion/plots/marg_uavsar_diff_box_v4.png")
-
-
-# calc mean, med, sd both hists
-hist_stats <- df1 %>%
-  group_by(data, pair) %>%
-  summarize(mean = mean(dswe, na.rm = T),
-            sd = sd(dswe, na.rm = T),
-            med = median(dswe, na.rm = T))
-
-f_labels <- data.frame(
-  pair = c("P1", "P2" ,"P3", "P4"),
-  label = c("(a) P1", "(b) P2" ,"(c) P3", "(d) P4"),
-  x = c(5,5,5,5),
-  y = c(3000,3000,3000,3000))
-
-
-dens_plot <-ggplot(df1, aes(x = dswe, fill = data,  after_stat(count))) +
-  geom_density(alpha = .5) +
-  facet_grid(vars(pair),scales = "fixed") +
-  geom_text(data = hist_stats, aes(x = x, y = y, label = label), vjust = 2) +
-  scale_fill_manual(values = c('UAVSAR' = 'goldenrod', 'WUS-SR' = 'purple2'))+
-  xlab(expression(Delta~SWE~(cm))) +
-  scale_x_continuous(expand = c(0,0), limits = c(-10,10)) +
-  scale_y_continuous(expand = c(.007,0), limits = c(0,3500)) +
-  ylab("Count") + 
-  theme(panel.border = element_rect(colour = "black", fill = NA, linewidth  = 1),
-        legend.position = c(.24,.92),
-        legend.direction = 'vertical',
-        legend.title = element_blank(),
-        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
-        plot.margin = unit(c(.25,.25, 0,.25), "cm"))
+# data.table::fwrite(df, "~/ch3_fusion/csvs/uavsar_marg_plotting_df_volume_v1.csv")
 
 # Calculate mean, median, and standard deviation for UAVSAR and WUS-SR
 summary_stats <- df1 %>%
   group_by(data, pair) %>%
   summarize(mean = mean(dswe, na.rm = T),
-            sd = sd(dswe, na.rm = T),
+            iqr = IQR(dswe, na.rm = T),
             med = median(dswe, na.rm = T))
 
+# filter
 uav_stats <-filter(summary_stats, data == "UAVSAR")
 wus_stats <-filter(summary_stats, data == "WUS-SR")
 
-dens_plot2 <- ggplot(df1, aes(x = dswe, fill = data, after_stat(count))) +
-  geom_density(alpha = 0.5) +
-  facet_grid(vars(pair), scales = "fixed") +
+round(summary_stats$iqr,2)
+
+# plot
+p1 <-ggplot(df1, aes(y = dswe, fill = data)) +
+  geom_hline(yintercept = 0, col = "gray50", linetype = 3)+
+  geom_boxplot(linewidth = .5, width = 1.5, 
+               outlier.shape = 1, outlier.color = 'red', outlier.alpha = .3, outlier.size = .2,
+               position = 'dodge') +
+  facet_wrap(vars(pair), scales = "fixed", ncol = 4) +
+  scale_fill_manual(values = c('UAVSAR' = 'goldenrod', 'WUS-SR' = 'purple2'))+
   geom_text(data = uav_stats, 
-            aes(label = paste0("Mean = ", round(mean, 2), " cm",
-                              "\nMedian = ", round(med, 2), " cm",
-                              "\nSD = ", round(sd, 2), " cm"), x = 4, y = 3200),
-                                hjust = 0, vjust = 1, size = 3, color = "goldenrod") +
+            aes(label = paste0("\nMedian = ", sprintf("%.2f", round(med,2)), " cm",
+                               "\nMean = ", round(mean, 2), " cm",
+                               "\nIQR = ", sprintf("%.2f", round(iqr,2)), " cm"),
+                x = 1.2, y = Inf),
+            hjust = 1, vjust = 1, size = 3, color = "goldenrod", fontface='bold') +
   geom_text(data = wus_stats, 
-            aes(label = paste0("Mean = ", round(mean, 2), " cm",
-                              "\nMedian = ", round(med, 2), " cm", 
-                              "\nSD = ", round(sd, 2), " cm"), x = 4, y = 1800), 
-            hjust = 0, vjust = 1, size = 3, color = "purple2") +
-  scale_fill_manual(values = c('UAVSAR' = 'goldenrod', 'WUS-SR' = 'purple2')) +
-  xlab(expression(Delta~SWE~(cm))) +
-  scale_x_continuous(expand = c(0, 0), limits = c(-10, 10)) +
-  scale_y_continuous(expand = c(0.007, 0), limits = c(0, 3500)) +
-  ylab("Count") +
-  theme(
-    panel.border = element_rect(colour = "black", fill = NA, linewidth = 1),
-    legend.position = c(0.24, 0.92),
-    legend.direction = 'vertical',
-    legend.title = element_blank(),
-    legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
-    plot.margin = unit(c(0.25, 0.25, 0, 0.25), "cm"))
+            aes(label = paste0("\nMedian = ", sprintf("%.2f", round(med,2)), " cm",
+                               "\nMean = ", round(mean, 2), " cm",
+                               "\nIQR = ", sprintf("%.2f", round(iqr,2)), " cm"),
+                x = 1.2, y = 8), 
+            hjust = 1, vjust = 1, size = 3, color = "purple2", fontface='bold') +
+  geom_text(data = wus_stats, 
+            aes(label = paste0(pair),x = -.8, y = 11), 
+            hjust = 1, vjust = 1, size = 6, color = "black") +
+  ylab(expression(Delta~SWE~(cm))) +
+  xlab("Pair") + 
+  xlim(-1.2,1.2) +
+  scale_y_continuous(limits = c(-10,11)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA, linewidth  = 1),
+        strip.background = element_blank(),
+        strip.text.x = element_blank(),
+        legend.position = c(.63,.10),
+        legend.direction = 'vertical',
+        legend.title = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.spacing = unit(0,'lines'),
+        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+        plot.margin = unit(c(.25,.25, 0,.25), "cm"))
 
-dens_plot2
-
-
-
-# theme(panel.border = element_blank(),
-#       axis.text.x = element_blank(),
-#       axis.title.y = element_blank(),
-#       axis.title.x = element_blank(),
-#       axis.text.y = element_blank(),
-#       axis.ticks = element_blank(),
-#       legend.position = "right",
-#       plot.margin = unit(c(0,0,0,0), "cm"),
-#       strip.background = element_blank(), 
-#       legend.box.spacing = unit(0, "pt"), 
-#       strip.text.y.left = element_text(angle = 0),
-#       strip.text.x.top = element_blank(),
-#       strip.text = element_text(size = 13, face = "bold")) +
-
-# dens_plot
-
-ggsave(dens_plot2,
-       file = "~/ch3_fusion/plots/marg_uavsar_diff_hist_v2.png",
-       width = 5, 
-       height = 6,
+ggsave(p1,
+       file = "~/ch3_fusion/plots/marg_uavsar_diff_box_v6.pdf",
+       width = 9, 
+       height = 4,
        units = "in",
        dpi = 500) 
 
-system("open ~/ch3_fusion/plots/marg_uavsar_diff_hist_v2.png")
+system("open ~/ch3_fusion/plots/marg_uavsar_diff_box_v6.pdf")
+
 
