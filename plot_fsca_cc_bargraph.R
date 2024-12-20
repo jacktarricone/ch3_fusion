95273ae38adf7f4f3cadf285f41189170028a882
Jack Tarricone <jack.tarricone@nasa.gov>
2024-12-19 23:57:06 -0500

    plotting

--- plot_fsca_cc_bargraph.R
+++ plot_fsca_cc_bargraph.R
@@ -61,7 +61,8 @@ freq(p1_stack[[3]])
 # bring in cc data
 cc_raw <-rast("./geo_layers/cc_domain.tif")
 cc1 <-resample(cc_raw, p1_stack)
-cc <-mask(cc1, p1_stack1[[1]]) # before nan remove stack
+cc_sa <-mask(cc1, p1_stack1[[1]]) # before nan remove stack
+plot(cc_sa)
 names(cc) <-("cc")
 cc <-ifel(is.na(cc), 0, cc)
 plot(cc)
@@ -89,22 +90,12 @@ data <- df1 %>%
       cc >= 30 & cc <= 45 ~ "30-45",
       cc >= 45 & cc <= 60 ~ "45-60",
       cc >= 60 & cc <= 100 ~ "60-100",
-      # cc >= 0 & cc <= 10  ~ "0-10",
-      # cc >= 10 & cc <= 20 ~ "10-20",
-      # cc >= 20 & cc <= 30 ~ "20-30",
-      # cc >= 30 & cc <= 40 ~ "30-40",      
-      # cc >= 40 & cc <= 50 ~ "40-50",
-      # cc >= 50 & cc <= 60 ~ "50-60",
-      # cc >= 60 & cc <= 70 ~ "60-70",
-      # cc >= 70 & cc <= 80 ~ "70-80",
-      # cc >= 80 & cc <= 90 ~ "80-90",
-      # cc >= 90 & cc <= 100 ~ "90-100",
     )
   )
 
 
 # Summarize total area by quantile
-summary1 <- data %>%
+summary <- data %>%
   group_by(cc_group) %>%
   summarize(
     FLM = sum(FLM, na.rm = TRUE),
@@ -116,8 +107,6 @@ summary1 <- data %>%
   )
 
 # Print the result
-print(summary1)
-summary <-summary1[-9,]
 summary
 
 plot_df1 <-pivot_longer(summary, 
@@ -128,9 +117,9 @@ plot_df1 <-pivot_longer(summary,
 #############
 ggplot(plot_df1, aes(fill= dataset, x = cc_group, y=value)) + 
   geom_bar(position="dodge", stat="identity", color = "black", width = .5)+
-  scale_y_continuous(expand = c(.001,.001), 
-                     limits = c(0,335),
-                     breaks = c(seq(0,300,100)))+
+  scale_y_continuous(expand = c(.004,0), 
+                     limits = c(0,700),
+                     breaks = c(seq(0,700,100)))+
   scale_fill_viridis_d(option = "turbo")+
   ylab(expression(SCA~(km^2))) +
   xlab("Canopy Cover (%)") +
@@ -145,25 +134,105 @@ ggplot(plot_df1, aes(fill= dataset, x = cc_group, y=value)) +
         axis.text.y = element_text(color = "black"),
         axis.text.x = element_text(color = "black"))
 
-ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v3.pdf",
+ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf",
        width = 5.5, 
        height = 3)
 
-system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v3.pdf") 
+system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf") 
 
 
 # calc cc area for bins
 # function
+
 ccArea <-function(cc_rast, lower,upper){
-  cc_v1 <-ifel(cc_rast >= lower, NA, cc)
-  cc_v2 <-ifel(cc_v1 <= upper, NA, cc)
+  cc_v1 <-ifel(cc_rast >= upper, NA, cc_rast)
+  cc_v2 <-ifel(cc_v1 <= lower, NA, cc_v1)
   df <-expanse(cc_v2, unit = "km")
   cc_area <-df$area[1]
 }
 
 # run
-cc15 <-ccArea(cc,0,15)
-cc30 <-ccArea(cc,15,30)
-cc45 <-ccArea(cc,30,45)
-cc60 <-ccArea(cc,45,60)
-cc100 <-ccArea(cc,60,100)
+cc15 <-ccArea(cc_sa,0,15)
+cc30 <-ccArea(cc_sa,15,30)
+cc45 <-ccArea(cc_sa,30,45)
+cc60 <-ccArea(cc_sa,45,60)
+cc100 <-ccArea(cc_sa,60,100)
+
+cc15_perc <-(cc15/area)*100
+cc30_perc <-(cc30/area)*100
+cc45_perc <-(cc45/area)*100
+cc60_perc <-(cc60/area)*100
+cc100_perc <-(cc100/area)*100
+
+
+shp <-vect("/Users/jtarrico/ch3_fusion/shapefiles/sierra_multiseg_shp_v4.gpkg")
+area <-expanse(shp, unit = "km")
+plot(p1_stack1[[1]])
+
+
+# make col
+just_string <-c(cc15,cc30,cc45,cc60,cc100)
+
+summary$cc_total_area <-just_string
+summary
+
+result <- summary %>% 
+  group_by(cc_group) %>%
+  mutate(
+    FLM = (FLM / cc_total_area)*100,
+    IMS = (IMS / cc_total_area)*100,
+    Landsat = (Landsat / cc_total_area)*100,
+    MODIS = (MODIS / cc_total_area)*100,
+    STC = (STC / cc_total_area)*100,
+    VIIRS = (VIIRS / cc_total_area)*100
+  )
+
+result
+
+result2 <- summary %>% 
+  group_by(cc_group) %>%
+  mutate(
+    FLM = (FLM / area)*100,
+    IMS = (IMS / area)*100,
+    Landsat = (Landsat / area)*100,
+    MODIS = (MODIS / area)*100,
+    STC = (STC / area)*100,
+    VIIRS = (VIIRS / area)*100
+  )
+
+result2
+
+# calc area stats
+cc15_perc <-(cc15/area)*100
+
+# plotting df
+plot_df2 <-pivot_longer(result, 
+                        cols = c("IMS","MODIS","VIIRS","STC","Landsat","FLM"),
+                        names_to = c("dataset"))
+
+ggplot(plot_df2, aes(fill= dataset, x = cc_group, y=value)) + 
+  geom_bar(position="dodge", stat="identity", color = "black", width = .5)+
+  scale_y_continuous(expand = c(.004,0), 
+                     limits = c(0,100),
+                     breaks = c(seq(0,100,20)))+
+  scale_fill_viridis_d(option = "turbo")+
+  ylab("SCA (%)") +
+  xlab("Canopy Cover (%)") +
+  theme(panel.border = element_rect(colour = "black", fill = NA, linewidth = 1),
+        legend.position = c(0.3, 0.83),
+        legend.text = element_text(size = 8),
+        legend.direction = "horizontal",
+        legend.justification = "center",
+        legend.box.just = "center", 
+        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+        legend.title = element_blank(),
+        axis.text.y = element_text(color = "black"),
+        axis.text.x = element_text(color = "black"))
+
+ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v2.pdf",
+       width = 5.5, 
+       height = 3)
+
+system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v2.pdf") 
+
+
