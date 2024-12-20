217095d6cc392f1e49fbf18b4cb0dbe1160fa2f5
Jack Tarricone <jack.tarricone@nasa.gov>
2024-12-20 00:28:26 -0500

    good plot

--- plot_fsca_cc_bargraph.R
+++ plot_fsca_cc_bargraph.R
@@ -46,9 +46,6 @@ setwd("~/ch3_fusion/rasters")
 p1_stack1 <-rast(list.files("./new_optical/p1_80m_20200131_20200212", pattern = "_80m", full.names = T))
 names(p1_stack1) <-c("FLM","IMS","Landsat","MODIS","STC","VIIRS")
 p1_stack <-ifel(is.na(p1_stack1), 0, p1_stack1)
-plot(p1_stack[[3]])
-freq(p1_stack[[3]])
-
 
 # p2_stack <-rast(list.files("./new_optical/p2_80m_20200212_20200219/", pattern = "_80m", full.names = T))
 # names(p2_stack) <-c("FLM","IMS","Landsat","MODIS","STC","VIIRS")
@@ -63,8 +60,8 @@ cc_raw <-rast("./geo_layers/cc_domain.tif")
 cc1 <-resample(cc_raw, p1_stack)
 cc_sa <-mask(cc1, p1_stack1[[1]]) # before nan remove stack
 plot(cc_sa)
-names(cc) <-("cc")
-cc <-ifel(is.na(cc), 0, cc)
+names(cc_sa) <-("cc")
+cc <-ifel(is.na(cc_sa), 0, cc_sa)
 plot(cc)
 
 # calc cell size in km
@@ -73,8 +70,8 @@ cell_size
 plot(cell_size)
 
 # convert fsca percent to area
-fsca_area <-(p1_stack/100)*cell_size
-plot(fsca_area[[3]])
+fsca_area <-mask((p1_stack/100)*cell_size,cc_sa)
+plot(fsca_area)
 
 # stack with cc
 full_stack <-c(cc,fsca_area)
@@ -86,10 +83,10 @@ data <- df1 %>%
   mutate(
     cc_group = case_when(
       cc >= 0 & cc <= 15 ~ "0-15",
-      cc >= 15 & cc <= 30  ~ "15-30",
-      cc >= 30 & cc <= 45 ~ "30-45",
-      cc >= 45 & cc <= 60 ~ "45-60",
-      cc >= 60 & cc <= 100 ~ "60-100",
+      cc > 15 & cc <= 30  ~ "15-30",
+      cc > 30 & cc <= 45 ~ "30-45",
+      cc > 45 & cc <= 60 ~ "45-60",
+      cc > 60 & cc <= 100 ~ "60-100",
     )
   )
 
@@ -134,11 +131,11 @@ ggplot(plot_df1, aes(fill= dataset, x = cc_group, y=value)) +
         axis.text.y = element_text(color = "black"),
         axis.text.x = element_text(color = "black"))
 
-ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf",
-       width = 5.5, 
-       height = 3)
-
-system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf") 
+# ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf",
+#        width = 5.5, 
+#        height = 3)
+# 
+# system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf") 
 
 
 # calc cc area for bins
@@ -146,29 +143,27 @@ system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_v4.pdf")
 
 ccArea <-function(cc_rast, lower,upper){
   cc_v1 <-ifel(cc_rast >= upper, NA, cc_rast)
-  cc_v2 <-ifel(cc_v1 <= lower, NA, cc_v1)
+  cc_v2 <-ifel(cc_v1 < lower, NA, cc_v1)
   df <-expanse(cc_v2, unit = "km")
-  cc_area <-df$area[1]
+  cc_area <-as.integer(df$area[1])
 }
 
-# run
+# calc area in km per bin
+shp <-vect("/Users/jtarrico/ch3_fusion/shapefiles/sierra_multiseg_shp_v4.gpkg")
+area <-expanse(shp, unit = "km")
+plot(cc_sa)
 cc15 <-ccArea(cc_sa,0,15)
 cc30 <-ccArea(cc_sa,15,30)
 cc45 <-ccArea(cc_sa,30,45)
 cc60 <-ccArea(cc_sa,45,60)
 cc100 <-ccArea(cc_sa,60,100)
 
-cc15_perc <-(cc15/area)*100
-cc30_perc <-(cc30/area)*100
-cc45_perc <-(cc45/area)*100
-cc60_perc <-(cc60/area)*100
-cc100_perc <-(cc100/area)*100
-
-
-shp <-vect("/Users/jtarrico/ch3_fusion/shapefiles/sierra_multiseg_shp_v4.gpkg")
-area <-expanse(shp, unit = "km")
-plot(p1_stack1[[1]])
-
+# calc percent
+cc15_perc <-round((cc15/area)*100,1)
+cc30_perc <-round((cc30/area)*100,1)
+cc45_perc <-round((cc45/area)*100,1)
+cc60_perc <-round((cc60/area)*100,1)
+cc100_perc <-round((cc100/area)*100,1)
 
 # make col
 just_string <-c(cc15,cc30,cc45,cc60,cc100)
@@ -189,50 +184,51 @@ result <- summary %>%
 
 result
 
-result2 <- summary %>% 
-  group_by(cc_group) %>%
-  mutate(
-    FLM = (FLM / area)*100,
-    IMS = (IMS / area)*100,
-    Landsat = (Landsat / area)*100,
-    MODIS = (MODIS / area)*100,
-    STC = (STC / area)*100,
-    VIIRS = (VIIRS / area)*100
-  )
-
-result2
-
-# calc area stats
-cc15_perc <-(cc15/area)*100
 
 # plotting df
 plot_df2 <-pivot_longer(result, 
                         cols = c("IMS","MODIS","VIIRS","STC","Landsat","FLM"),
                         names_to = c("dataset"))
 
+# reorganize for plotting
+plot_df2$dataset <-factor(plot_df2$dataset, 
+                               levels=c("IMS","MODIS","VIIRS","STC","Landsat","FLM"))
+
+
 ggplot(plot_df2, aes(fill= dataset, x = cc_group, y=value)) + 
   geom_bar(position="dodge", stat="identity", color = "black", width = .5)+
   scale_y_continuous(expand = c(.004,0), 
-                     limits = c(0,100),
+                     limits = c(0,115),
                      breaks = c(seq(0,100,20)))+
   scale_fill_viridis_d(option = "turbo")+
   ylab("SCA (%)") +
   xlab("Canopy Cover (%)") +
+  annotate("text", x = "0-15", y = 106, 
+           label = bquote(.(cc15) ~ km^2 ~ "(" * .(cc15_perc) * " %)"), size = 2.5) +
+  annotate("text", x = "15-30", y = 106, 
+           label = bquote(.(cc30) ~ km^2 ~ "(" * .(cc30_perc) * " %)"), size = 2.5) +
+  annotate("text", x = "30-45", y = 106, 
+           label = bquote(.(cc45) ~ km^2 ~ "(" * .(cc45_perc) * " %)"), size = 2.5) +
+  annotate("text", x = "45-60", y = 106, 
+           label = bquote(.(cc60) ~ km^2 ~ "(" * .(cc60_perc) * " %)"), size = 2.5) +
+  annotate("text", x = "60-100", y = 106, 
+           label = bquote(.(cc100) ~ km^2 ~ "(" * .(cc100_perc) * " %)"), size = 2.6) +
   theme(panel.border = element_rect(colour = "black", fill = NA, linewidth = 1),
-        legend.position = c(0.3, 0.83),
+        legend.position = "top",
         legend.text = element_text(size = 8),
         legend.direction = "horizontal",
         legend.justification = "center",
         legend.box.just = "center", 
-        legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+        legend.margin = margin(t = 0, r = 0, b = -10, l = 0),
         legend.title = element_blank(),
         axis.text.y = element_text(color = "black"),
-        axis.text.x = element_text(color = "black"))
+        axis.text.x = element_text(color = "black"))+
+  guides(fill = guide_legend(nrow = 1)) 
 
-ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v2.pdf",
+ggsave(file = "~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v3.pdf",
        width = 5.5, 
        height = 3)
 
-system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v2.pdf") 
+system("open ~/ch3_fusion/plots/fig8_fsca_cc_bargraph_percent_v3.pdf") 
 
 
